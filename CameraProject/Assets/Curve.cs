84edd75b3cefe33eb2bef4a1f5327e302bfadfe5
Cedric CHAUVIN <cedric.chauvin@edu.devinci.fr>
2021-01-28 10:15:26 +0100

    Curve

---
+++ CameraProject/Assets/Curve.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curve : MonoBehaviour
+{
+    
+    public Vector3 A,B,C,D = Vector3.zero;
+
+    Vector3 GetPosition(float t) => MathUtils.CubicBezier(A, B, C, D, t);
+
+    Vector3 GetPosition(float t, Matrix4x4 localToWorldMatrix)
+    {
+        Vector3 pLocal = GetPosition(t);
+        Vector3 pWorld = localToWorldMatrix.MultiplyPoint(pLocal);
+        return pWorld;
+    }
+
+    void DrawGizmo(Color c, Matrix4x4 localToWorldMatrix)
+    {
+        Gizmos.color = c;
+        Gizmos.DrawSphere(localToWorldMatrix.MultiplyPoint(A),0.25f);
+        Gizmos.DrawSphere(localToWorldMatrix.MultiplyPoint(B),0.25f);
+        Gizmos.DrawSphere(localToWorldMatrix.MultiplyPoint(C),0.25f);
+        Gizmos.DrawSphere(localToWorldMatrix.MultiplyPoint(D),0.25f);
+        Vector3 previousPoint = Vector3.zero;
+        for (float t = 0; t < 1; t+=0.1f)
+        {
+            Vector3 point = GetPosition(t, localToWorldMatrix);
+            if(previousPoint!=Vector3.zero)
+                Gizmos.DrawLine(previousPoint,point);
+            previousPoint = point;
+        }
+        Gizmos.DrawLine(previousPoint,GetPosition(1,localToWorldMatrix));
+    }
+
+    private void OnDrawGizmos()
+    {
+        DrawGizmo(Color.red, transform.localToWorldMatrix);
+    }
+}
--- CameraProject/Assets/MathUtils.cs
+++ CameraProject/Assets/MathUtils.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class MathUtils : MonoBehaviour
 {
 
-    Vector3 LinearBezier(Vector3 A, Vector3 B, float t)
+    public static Vector3 LinearBezier(Vector3 A, Vector3 B, float t)
     {
         return (1 - t) * A + t * B;
     }
 
-    Vector3 QuadratiqueBezier(Vector3 A, Vector3 B, Vector3 C, float t)
+    public static Vector3 QuadratiqueBezier(Vector3 A, Vector3 B, Vector3 C, float t)
     {
         return (1 - t) * LinearBezier(A, B, t) + t * LinearBezier(B, C, t);
     }
 
-    Vector3 CubicBezier(Vector3 A, Vector3 B, Vector3 C,Vector3 D, float t)
+    public static Vector3 CubicBezier(Vector3 A, Vector3 B, Vector3 C,Vector3 D, float t)
     {
         return (1 - t) * QuadratiqueBezier(A, B, C, t) + t * QuadratiqueBezier(B, C, D, t);
     }
